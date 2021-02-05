0ab37ba8802bb3c3f956f5d416a2a5d01866f179
lucascost <lucas.yan97@outlook.com>
2021-02-05 14:33:24 -0300

    Frases adicionadas; Finalizado

--- Frase_do_dia/app/src/main/java/com/example/frasedodia/MainActivity.kt
+++ Frase_do_dia/app/src/main/java/com/example/frasedodia/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.frasedodia
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var texto: TextView
+    val frases = arrayOf(
+        "Olhe pra cima, que é de lá que vem tua força!",
+        "Agora é hora de começar a surpreender aqueles que duvidaram de voce!",
+        "Você nunca será velho demais para sonhar um novo sonho.",
+        "Quando pensar em desistir, lembre-se porque começou."
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        texto = findViewById(R.id.txtFrase)
     }
+
+    fun gerarFrase(view: View){
+        val totalItens = frases.size
+        val numero = Random().nextInt(totalItens)
+        texto.text = frases[numero]
+    }
+
 }
