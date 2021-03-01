a20b7c308ad18db0b13294230e04d60f4ff09191
lucascost <lucas.yan97@outlook.com>
2021-03-01 10:59:23 -0300

    .

--- Notas/app/src/main/java/com/example/notas/MainActivity.kt
+++ Notas/app/src/main/java/com/example/notas/MainActivity.kt
@@ -8,7 +8,10 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
 
+
 class MainActivity : AppCompatActivity() {
+    lateinit var nota_atual:Nota
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,18 +20,44 @@ class MainActivity : AppCompatActivity() {
             .allowMainThreadQueries().build()
         val notas: List<Nota> = db.noteDao().listar()
 
+        var acao = 1 //1-salvar 2-editar 3-excluir
+        var id = 0
+
         val rcv = findViewById<RecyclerView>(R.id.recycler)
+        val edt = findViewById<EditText>(R.id.edtText)
+        val btn = findViewById<Button>(R.id.button)
+
         rcv.adapter = NoteAdapter(notas)
         rcv.layoutManager = LinearLayoutManager(this)
 
-        this.findViewById<Button>(R.id.button).setOnClickListener() {
-            val n = Nota()
-            val edt = findViewById<EditText>(R.id.edtText)
-            n.note_text = edt.text.toString()
-            db.noteDao().inserir(n)
+
+
+
+        rcv.addOnItemTouchListener(RecyclerItemClickListener(this) { view, position ->
+            mudaTexto(edt, db,position)
+            acao=2
+            id = position+1
+            btn.setText("Salvar")
+        })
+
+        btn.setOnClickListener() {
+            if(acao==1) {
+                nota_atual = Nota()
+                nota_atual.note_text = edt.text.toString()
+                db.noteDao().inserir(nota_atual)
+            }
+            else{
+                nota_atual= db.noteDao().procurar(id)
+                nota_atual.note_text= edt.text.toString()
+                db.noteDao().editar(nota_atual)
+            }
             edt.setText("")
             this.recreate();
 
         }
+    }
+    fun mudaTexto(edt: EditText, db: DBase,pos:Int){
+        val n = db.noteDao().procurar(pos+1)
+        edt.setText(n.note_text)
     }
 }
--- Notas/app/src/main/java/com/example/notas/NoteAdapter.kt
+++ Notas/app/src/main/java/com/example/notas/NoteAdapter.kt
@@ -1,14 +1,17 @@
 package com.example.notas
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class NoteAdapter(var lista:List<Nota>):RecyclerView.Adapter<NoteAdapter.ViewHolder>(){
     class ViewHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
         val txtView = itemView.findViewById<TextView>(R.id.textView)
+        val layout = itemView.findViewById<LinearLayout>(R.id.r)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -19,6 +22,11 @@ class NoteAdapter(var lista:List<Nota>):RecyclerView.Adapter<NoteAdapter.ViewHol
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val n = lista[position]
         holder.txtView.text = n.note_text
+        holder.layout.setOnClickListener(){
+            val i = Intent(holder.itemView.context, MainActivity::class.java)
+            i.putExtra("pos",position)
+
+        }
     }
 
     override fun getItemCount(): Int {
