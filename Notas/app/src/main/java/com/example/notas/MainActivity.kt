b0fc54369297ece559da4f7a4f1834d36e7d0ec2
lucascost <lucas.yan97@outlook.com>
2021-03-02 14:53:05 -0300

    funções finalizadas

--- Notas/app/src/main/java/com/example/notas/MainActivity.kt
+++ Notas/app/src/main/java/com/example/notas/MainActivity.kt
@@ -4,60 +4,67 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var nota_atual:Nota
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val db: DBase = Room.databaseBuilder(applicationContext, DBase::class.java, "banco")
-            .allowMainThreadQueries().build()
-        val notas: List<Nota> = db.noteDao().listar()
-
-        var acao = 1 //1-salvar 2-editar 3-excluir
-        var id = 0
+                .allowMainThreadQueries().build()
+        var notas: List<Nota> = db.noteDao().listar()
+        var acao = 1
+        var nota_atual:Nota = Nota()
 
         val rcv = findViewById<RecyclerView>(R.id.recycler)
         val edt = findViewById<EditText>(R.id.edtText)
         val btn = findViewById<Button>(R.id.button)
+        val btnDel = findViewById<Button>(R.id.button2)
+            btnDel.isVisible=false
+
 
         rcv.adapter = NoteAdapter(notas)
         rcv.layoutManager = LinearLayoutManager(this)
 
 
-
-
         rcv.addOnItemTouchListener(RecyclerItemClickListener(this) { view, position ->
-            mudaTexto(edt, db,position)
+            nota_atual=notas[position]
+            edt.setText(nota_atual.note_text)
+            btn.text="Salvar"
             acao=2
-            id = position+1
-            btn.setText("Salvar")
+            btnDel.isVisible=true
         })
 
         btn.setOnClickListener() {
-            if(acao==1) {
-                nota_atual = Nota()
-                nota_atual.note_text = edt.text.toString()
-                db.noteDao().inserir(nota_atual)
-            }
-            else{
-                nota_atual= db.noteDao().procurar(id)
-                nota_atual.note_text= edt.text.toString()
-                db.noteDao().editar(nota_atual)
-            }
-            edt.setText("")
-            this.recreate();
+                if(!edt.text.isNullOrEmpty()){
+                    nota_atual.note_text=edt.text.toString()
 
+                    if(acao==1){
+                        db.noteDao().inserir(nota_atual)
+                        this.recreate()
+                    }
+                    else
+                        db.noteDao().editar(nota_atual)
+                } else {
+                    edt.error="campo vazio."
+                }
+            rcv.adapter?.notifyDataSetChanged()
+            nota_atual=Nota()
+            acao=1
+            edt.setText("")
+            btn.text="Adicionar"
+            btnDel.isVisible=false
         }
-    }
-    fun mudaTexto(edt: EditText, db: DBase,pos:Int){
-        val n = db.noteDao().procurar(pos+1)
-        edt.setText(n.note_text)
+        btnDel.setOnClickListener(){
+            db.noteDao().remover(nota_atual)
+            edt.setText("")
+            this.recreate()
+        }
+
+
     }
 }
--- Notas/app/src/main/java/com/example/notas/NoteAdapter.kt
+++ Notas/app/src/main/java/com/example/notas/NoteAdapter.kt
@@ -1,32 +1,26 @@
 package com.example.notas
 
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class NoteAdapter(var lista:List<Nota>):RecyclerView.Adapter<NoteAdapter.ViewHolder>(){
     class ViewHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
         val txtView = itemView.findViewById<TextView>(R.id.textView)
-        val layout = itemView.findViewById<LinearLayout>(R.id.r)
+        //val layout = itemView.findViewById<LinearLayout>(R.id.layout)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.note_row,parent,false)
         return ViewHolder(v)
+
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val n = lista[position]
         holder.txtView.text = n.note_text
-        holder.layout.setOnClickListener(){
-            val i = Intent(holder.itemView.context, MainActivity::class.java)
-            i.putExtra("pos",position)
-
-        }
     }
 
     override fun getItemCount(): Int {
