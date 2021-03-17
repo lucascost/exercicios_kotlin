8877a1fba63d951165ff360d40d3e60733d1dad7
lucascost <lucas.yan97@outlook.com>
2021-03-17 11:26:05 -0300

    botton navigation adicionado

--- Fragments/app/src/main/java/com/example/fragments/MainActivity.kt
+++ Fragments/app/src/main/java/com/example/fragments/MainActivity.kt
@@ -1,28 +1,31 @@
 package com.example.fragments
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
 
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setFragment(home_fragment())
 
-        this.findViewById<Button>(R.id.button).setOnClickListener(){
-            setFragment(home_fragment())
-        }
-        this.findViewById<Button>(R.id.button2).setOnClickListener(){
-            setFragment(second_fragment())
-        }
-        this.findViewById<Button>(R.id.button3).setOnClickListener(){
-            setFragment(third_fragment())
-        }
+        val bottonNavHome = findViewById<BottomNavigationItemView>(R.id.menu_home)
+        val bottonNavSec = findViewById<BottomNavigationItemView>(R.id.menu_second)
+        val bottonNavThird = findViewById<BottomNavigationItemView>(R.id.menu_third)
+
+        bottonNavHome.setOnClickListener(){ setFragment(home_fragment()) }
+        bottonNavSec.setOnClickListener() { setFragment(second_fragment()) }
+        bottonNavThird.setOnClickListener(){ setFragment(third_fragment()) }
+
     }
     private fun setFragment(fragment:Fragment){
         val manager = supportFragmentManager
